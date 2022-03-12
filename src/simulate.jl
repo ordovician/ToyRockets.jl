04ea379dcc5d7098942be0318d3f9583fc2104f2
Erik Engheim <ordovician.cenozoic@mac.com>
2022-03-12 18:54:22 +0100

    Add callback to launch!
    Motivation: Make it easier to collect values for later plotting

--- src/simulate.jl
+++ src/simulate.jl
@@ -8,7 +8,7 @@ and time it was achieved
 function launch!(rocket::Rocket, Δt::Real)
     g = 9.80665  # acceleration caused by gravity
     v = 0        # velocity
-    r = 0        # altitude
+    h = 0        # altitude
     
     for t in 0:Δt:1000
         m = mass(rocket)
@@ -18,12 +18,41 @@ function launch!(rocket::Rocket, Δt::Real)
 
         # Any propellant and thrust left?
         if remaining == 0 || F <= 0
-            return (t, r)
+            return (t, h)
         end
         
-        r += v*Δt
+        h += v*Δt
         a = F/m
         v += a*Δt
     end
 end
 
+"""
+    launch!(callback::Function, rocket::Rocket, Δt::Real) -> Float64
+Calls `callback(rocket, t, v, h)` on every iteration,
+where `v` and `h` is the velocity and altitude at time `t`.
+"""
+function launch!(callback::Function, rocket::Rocket, Δt::Real)
+    g = 9.80665  # acceleration caused by gravity
+    v = 0        # velocity
+    h = 0        # altitude
+    
+    for t in 0:Δt:1000
+        callback(rocket, t, v, h)
+        m = mass(rocket)
+        F = thrust(rocket) - m*g
+
+        remaining = update!(rocket, t, Δt)        
+
+        # Any propellant and thrust left?
+        if remaining == 0 || F <= 0
+            return (t, h)
+        end
+        
+        h += v*Δt
+        a = F/m
+        v += a*Δt        
+    end
+    callback(rocket, t, v, h)
+    nothing
+end
